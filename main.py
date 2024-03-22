77f917729a774fef559acdf159465e4740154dc9
Ashish Aman <129578796+ashu-317@users.noreply.github.com>
2024-03-22 12:09:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import streamlit as st
+import os
+from PIL import Image
+import numpy as np
+import pickle
 import tensorflow
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
-import numpy as np
+from sklearn.neighbors import NearestNeighbors
 from numpy.linalg import norm
-import os
-from tqdm import tqdm
-import pickle
+
+feature_list = np.array(pickle.load(open('embeddings.pkl','rb')))
+filenames = pickle.load(open('filenames.pkl','rb'))
 
 model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
 model.trainable = False
@@ -16,10 +21,18 @@ model = tensorflow.keras.Sequential([
     GlobalMaxPooling2D()
 ])
 
-#print(model.summary())
+st.title('Fashion Recommender System')
 
-def extract_features(img_path,model):
-    img = image.load_img(img_path,target_size=(224,224))
+def save_uploaded_file(uploaded_file):
+    try:
+        with open(os.path.join('uploads',uploaded_file.name),'wb') as f:
+            f.write(uploaded_file.getbuffer())
+        return 1
+    except:
+        return 0
+
+def feature_extraction(img_path,model):
+    img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     expanded_img_array = np.expand_dims(img_array, axis=0)
     preprocessed_img = preprocess_input(expanded_img_array)
@@ -28,15 +41,39 @@ def extract_features(img_path,model):
 
     return normalized_result
 
-filenames = []
+def recommend(features,feature_list):
+    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+    neighbors.fit(feature_list)
 
-for file in os.listdir('images'):
-    filenames.append(os.path.join('images',file))
+    distances, indices = neighbors.kneighbors([features])
 
-feature_list = []
+    return indices
 
-for file in tqdm(filenames):
-    feature_list.append(extract_features(file,model))
+# steps
+# file upload -> save
+uploaded_file = st.file_uploader("Choose an image")
+if uploaded_file is not None:
+    if save_uploaded_file(uploaded_file):
+        # display the file
+        display_image = Image.open(uploaded_file)
+        st.image(display_image)
+        # feature extract
+        features = feature_extraction(os.path.join("uploads",uploaded_file.name),model)
+        #st.text(features)
+        # recommendention
+        indices = recommend(features,feature_list)
+        # show
+        col1,col2,col3,col4,col5 = st.beta_columns(5)
 
-pickle.dump(feature_list,open('embeddings.pkl','wb'))
-pickle.dump(filenames,open('filenames.pkl','wb'))
+        with col1:
+            st.image(filenames[indices[0][0]])
+        with col2:
+            st.image(filenames[indices[0][1]])
+        with col3:
+            st.image(filenames[indices[0][2]])
+        with col4:
+            st.image(filenames[indices[0][3]])
+        with col5:
+            st.image(filenames[indices[0][4]])
+    else:
+        st.header("Some error occured in file upload")
